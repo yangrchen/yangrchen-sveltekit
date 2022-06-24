0aa5564a0e184d9dc964410a67033a154a864b36
Yang Chen <yyangc88@gmail.com>
2022-06-24 11:53:57 -0400

    Minimizing image file size

--- src/lib/data/drawings.ts
+++ src/lib/data/drawings.ts
@@ -1,9 +1,9 @@
-import AW1 from '$artwork/features-ear-study.png?width=1366;768;366&aspect=0.75&webp&srcset&imagetools';
-import AW2 from '$artwork/features-eye-study.png?width=1366;768;366&aspect=0.75&webp&srcset&imagetools';
-import AW3 from '$artwork/female-figure-structure.png?width=1366;768;366&aspect=0.75&webp&srcset&imagetools';
-import AW4 from '$artwork/male-head-profile.png?width=1366;768;366&aspect=0.75&webp&srcset&imagetools';
-import AW5 from '$artwork/simplified-head-planes-study.png?width=1366;768;366&aspect=0.75&webp&srcset&imagetools';
-import AW6 from '$artwork/skull-front-study.png?width=1366;768;366&aspect=0.75&webp&srcset&imagetools';
+import AW1 from '$artwork/features-ear-study.png?width=1366;768;366&aspect=0.75&webp&quality=20&srcset&imagetools';
+import AW2 from '$artwork/features-eye-study.png?width=1366;768;366&aspect=0.75&webp&quality=20&srcset&imagetools';
+import AW3 from '$artwork/female-figure-structure.png?width=1366;768;366&aspect=0.75&webp&quality=20&srcset&imagetools';
+import AW4 from '$artwork/male-head-profile.png?width=1366;768;366&aspect=0.75&webp&quality=20&srcset&imagetools';
+import AW5 from '$artwork/simplified-head-planes-study.png?width=1366;768;366&aspect=0.75&webp&quality=20&srcset&imagetools';
+import AW6 from '$artwork/skull-front-study.png?width=1366;768;366&aspect=0.75&webp&quality=20&srcset&imagetools';
 
 export default [
     {
