61d691b7d83d455b246ad0012740843a39b19262
Yang Chen <yyangc88@gmail.com>
2022-09-19 15:48:27 -0400

    Updated drawings 09.19.22

--- src/lib/data/drawings.ts
+++ src/lib/data/drawings.ts
@@ -4,8 +4,38 @@ import AW3 from '$artwork/female-figure-structure.png?width=1366;768;366&aspect=
 import AW4 from '$artwork/male-head-profile.png?width=1366;768;366&aspect=0.75&webp&quality=20&srcset&imagetools';
 import AW5 from '$artwork/simplified-head-planes-study.png?width=1366;768;366&aspect=0.75&webp&quality=20&srcset&imagetools';
 import AW6 from '$artwork/skull-front-study.png?width=1366;768;366&aspect=0.75&webp&quality=20&srcset&imagetools';
+import AW7 from '$artwork/female-figure-layin.png?width=1366;768;366&aspect=0.75&webp&quality=60&srcset&imagetools'
+import AW8 from '$artwork/female-head-shadow-mapping.png?width=1366;768;366&aspect=0.75&webp&quality=20&srcset&imagetools'
+import AW9 from '$artwork/female-head-2-value.png?width=1366;768;366&aspect=0.75&webp&quality=20&srcset&imagetools'
+import AW10 from '$artwork/female-head-shadow-mapping-2.png?width=1366;768;366&aspect=0.75&webp&quality=20&srcset&imagetools'
+import AW11 from '$artwork/female-head-shadow-mapping-1.png?width=1366;768;366&aspect=0.75&webp&quality=20&srcset&imagetools'
 
 export default [
+    {
+        "data-src": "",
+        "data-srcset": AW7,
+        alt: "Charcoal layin drawing of a female front pose"
+    },
+    {
+        "data-src": "",
+        "data-srcset": AW8,
+        alt: "Charcoal drawing of a female head with long hair in profile view"
+    },
+    {
+        "data-src": "",
+        "data-srcset": AW9,
+        alt: "Charcoal drawing of a female head with long hair in profile view with shading"
+    },
+    {
+        "data-src": "",
+        "data-srcset": AW10,
+        alt: "Charcoal drawing of a female head in three quarter view with eyes"
+    },
+    {
+        "data-src": "",
+        "data-srcset": AW11,
+        alt: "Charcoal drawing of a female head in three quarter view without eyes"
+    },
     {
         "data-src": "",
         "data-srcset": AW1,
