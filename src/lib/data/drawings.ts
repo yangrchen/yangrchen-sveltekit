0cd3dcc9b2c580daefe1bec7ceacccc5e7e81509
yangrchen <yangrc.chen@gmail.com>
2023-01-19 10:14:21 -0500

    Updated artwork

--- src/lib/data/drawings.ts
+++ src/lib/data/drawings.ts
@@ -1,9 +1,10 @@
-import AW1 from '$artwork/alia-lifedrawing-1.png?width=1366;768;366&aspect=0.75&webp&quality=60&srcset&imagetools';
-import AW2 from '$artwork/female-figure-layin.png?width=1366;768;366&aspect=0.75&webp&quality=60&srcset&imagetools'
-import AW3 from '$artwork/female-head-shadow-mapping.png?width=1366;768;366&aspect=0.75&webp&quality=20&srcset&imagetools'
-import AW4 from '$artwork/female-head-2-value.png?width=1366;768;366&aspect=0.75&webp&quality=20&srcset&imagetools'
-import AW5 from '$artwork/female-head-shadow-mapping-2.png?width=1366;768;366&aspect=0.75&webp&quality=20&srcset&imagetools'
-import AW6 from '$artwork/female-head-shadow-mapping-1.png?width=1366;768;366&aspect=0.75&webp&quality=20&srcset&imagetools'
+import AW0 from '$artwork/FAS-head-shadowmapping.png?width=1366;768;366&aspect=0.75&webp&quality=60&srcset&imagetools';
+import AW1 from '$artwork/watts-head-2-value.png?width=1366;768;366&aspect=0.75&webp&quality=60&srcset&imagetools';
+import AW2 from '$artwork/alia-lifedrawing-1.png?width=1366;768;366&aspect=0.75&webp&quality=60&srcset&imagetools';
+import AW3 from '$artwork/female-head-shadow-mapping.png?width=1366;768;366&aspect=0.75&webp&quality=20&srcset&imagetools';
+import AW4 from '$artwork/female-head-2-value.png?width=1366;768;366&aspect=0.75&webp&quality=20&srcset&imagetools';
+import AW5 from '$artwork/female-head-shadow-mapping-2.png?width=1366;768;366&aspect=0.75&webp&quality=20&srcset&imagetools';
+import AW6 from '$artwork/female-head-shadow-mapping-1.png?width=1366;768;366&aspect=0.75&webp&quality=20&srcset&imagetools';
 import AW7 from '$artwork/features-ear-study.png?width=1366;768;366&aspect=0.75&webp&quality=20&srcset&imagetools';
 import AW8 from '$artwork/features-eye-study.png?width=1366;768;366&aspect=0.75&webp&quality=20&srcset&imagetools';
 import AW9 from '$artwork/female-figure-structure.png?width=1366;768;366&aspect=0.75&webp&quality=20&srcset&imagetools';
@@ -12,78 +13,83 @@ import AW11 from '$artwork/simplified-head-planes-study.png?width=1366;768;366&a
 import AW12 from '$artwork/skull-front-study.png?width=1366;768;366&aspect=0.75&webp&quality=20&srcset&imagetools';
 
 const drawings: DrawingProps[] = [
-    {
-        srcset: AW1,
-        alt: "Charcoal drawing of a plump woman sitting with shadow mapping",
-        displayName: 'Life Drawing @ Alia Fine Art Studios',
-        medium: 'Charcoal Pencil'
-    },
-    {
-        srcset: AW2,
-        alt: "Charcoal layin drawing of a female front pose",
-        displayName: '20 Min. Female Layin Study',
-        medium: 'Charcoal Pencil'
-    },
-    {
-        srcset: AW3,
-        alt: "Charcoal drawing of a female head with long hair in profile view",
-        displayName: 'Female Profile Head Layin w/ Shadow Mapping',
-        medium: 'Charcoal Pencil'
-    },
-    {
-        srcset: AW4,
-        alt: "Charcoal drawing of a female head with long hair in profile view with shading",
-        displayName: 'Female Profile Two Value Layin',
-        medium: 'Charcoal Pencil'
-    },
-    {
-        srcset: AW5,
-        alt: "Charcoal drawing of a female head in three quarter view with eyes",
-        displayName: 'Female 3/4 Layin w/ Shadow Mapping',
-        medium: 'Charcoal Pencil'
-    },
-    {
-        srcset: AW6,
-        alt: "Charcoal drawing of a female head in three quarter view without eyes",
-        displayName: 'Female 3/4 Layin w/ Shadow Mapping 2',
-        medium: 'Charcoal Pencil'
-    },
-    {
-        srcset: AW7,
-        alt: "Charcoal drawing of an ear on newsprint paper",
-        displayName: 'Ear Master Study (Brian Knox)',
-        medium: 'Charcoal Pencil'
-    },
-    {
-        srcset: AW8,
-        alt: "Charcoal drawing of an eye on newsprint paper",
-        displayName: 'Eyes Master Study (Brian Knox)',
-        medium: 'Charcoal Pencil'
-    },
-    {
-        srcset: AW9,
-        alt: "Charcoal drawing of a female figure on newsprint paper",
-        displayName: 'Female Structural Figure Layin',
-        medium: 'Charcoal Pencil'
-    },
-    {
-        srcset: AW10,
-        alt: "Charcoal drawing of male head in profile on newsprint paper",
-        displayName: 'Male Profile Head Layin',
-        medium: 'Charcoal Pencil'
-    },
-    {
-        srcset: AW11,
-        alt: "Charcoal drawing of a planar head on newsprint paper",
-        displayName: 'Simplified Asaro Head Drawing',
-        medium: 'Charcoal Pencil'
-    },
-    {
-        srcset: AW12,
-        alt: "Charcoal drawing of a front view skull on newsprint paper",
-        displayName: 'Front View Skull Drawing',
-        medium: 'Charcoal Pencil'
-    }
-
-]
+	{
+		srcset: AW0,
+		alt: 'Charcoal drawing of a woman with a hairdress with shadow mapping',
+		displayName: 'FAS Female Head w/ Shadow Mapping',
+		medium: 'Charcoal Pencil'
+	},
+	{
+		srcset: AW1,
+		alt: 'Charcoal drawing of a woman with long braided hair with shading',
+		displayName: 'Watts Two Value Head Drawing',
+		medium: 'Charcoal Pencil'
+	},
+	{
+		srcset: AW2,
+		alt: 'Charcoal drawing of a plump woman sitting with shadow mapping',
+		displayName: 'Life Drawing @ Alia Fine Art Studios',
+		medium: 'Charcoal Pencil'
+	},
+	{
+		srcset: AW3,
+		alt: 'Charcoal drawing of a female head with long hair in profile view',
+		displayName: 'Female Profile Head Layin w/ Shadow Mapping',
+		medium: 'Charcoal Pencil'
+	},
+	{
+		srcset: AW4,
+		alt: 'Charcoal drawing of a female head with long hair in profile view with shading',
+		displayName: 'Female Profile Two Value Layin',
+		medium: 'Charcoal Pencil'
+	},
+	{
+		srcset: AW5,
+		alt: 'Charcoal drawing of a female head in three quarter view with eyes',
+		displayName: 'Female 3/4 Layin w/ Shadow Mapping',
+		medium: 'Charcoal Pencil'
+	},
+	{
+		srcset: AW6,
+		alt: 'Charcoal drawing of a female head in three quarter view without eyes',
+		displayName: 'Female 3/4 Layin w/ Shadow Mapping 2',
+		medium: 'Charcoal Pencil'
+	},
+	{
+		srcset: AW7,
+		alt: 'Charcoal drawing of an ear on newsprint paper',
+		displayName: 'Ear Master Study (Brian Knox)',
+		medium: 'Charcoal Pencil'
+	},
+	{
+		srcset: AW8,
+		alt: 'Charcoal drawing of an eye on newsprint paper',
+		displayName: 'Eyes Master Study (Brian Knox)',
+		medium: 'Charcoal Pencil'
+	},
+	{
+		srcset: AW9,
+		alt: 'Charcoal drawing of a female figure on newsprint paper',
+		displayName: 'Female Structural Figure Layin',
+		medium: 'Charcoal Pencil'
+	},
+	{
+		srcset: AW10,
+		alt: 'Charcoal drawing of male head in profile on newsprint paper',
+		displayName: 'Male Profile Head Layin',
+		medium: 'Charcoal Pencil'
+	},
+	{
+		srcset: AW11,
+		alt: 'Charcoal drawing of a planar head on newsprint paper',
+		displayName: 'Simplified Asaro Head Drawing',
+		medium: 'Charcoal Pencil'
+	},
+	{
+		srcset: AW12,
+		alt: 'Charcoal drawing of a front view skull on newsprint paper',
+		displayName: 'Front View Skull Drawing',
+		medium: 'Charcoal Pencil'
+	}
+];
 export default drawings;
